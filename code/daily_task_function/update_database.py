a89f357127e9e86c2dea7145fb94295da908f25f
Krittipoom <krittipoom@outlook.com>
2023-10-10 14:16:53 +0700

    fix update function

--- code/daily_task_function/update_database.py
+++ code/daily_task_function/update_database.py
@@ -8,6 +8,13 @@ sys.path.append(code)
 from function.get_and_add_data import ScrapDataFromGithub, AddNewDataToSQLite
 import time
 
+def UpdateUserData(updated_data, cur):
+	username, profile_img, last_year_contrib, total_stars, used_language_count_dict = updated_data
+	cur.execute(
+		"UPDATE saved_profile SET image = ?, contrib = ?, all_stars = ?, all_lang = ? WHERE usr = ?", 
+		(profile_img, last_year_contrib, total_stars, used_language_count_dict, username)
+		)
+
 def UpdateSQLiteDatabase():
 	start_time = time.time()
 	con = sqlite3.connect("requested_data.sqlite", check_same_thread=False)
@@ -16,7 +23,7 @@ def UpdateSQLiteDatabase():
 	usernames = [row[0] for row in cur.fetchall()]
 	for username in usernames:
 		github_data = ScrapDataFromGithub(username)
-		AddNewDataToSQLite(username, github_data)
+		UpdateUserData(github_data, cur)
 		print(f"Data updated for {username}")
 	end_time = time.time()
 	print(f"Total time taken: {end_time-start_time:.2f} seconds")
